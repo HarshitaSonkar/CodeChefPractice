c2c07b3ee143ba96c96f49a0f5799361bf707088
HarshitaSonkar <58362202+HarshitaSonkar@users.noreply.github.com>
2020-03-20 23:15:07 +0530

    Create Packaging Cupcakes.cpp

---
+++ Packaging Cupcakes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int num;
+	    cin>>num;
+	    cout<<(num/2)+1<<endl;
+	}
+	return 0;
+}
