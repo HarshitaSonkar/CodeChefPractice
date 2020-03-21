14c638b5feb3e68e204f070744164be080c6a598
HarshitaSonkar <58362202+HarshitaSonkar@users.noreply.github.com>
2020-03-21 15:08:25 +0530

    Create Chef and Remissness.cpp

---
+++ Chef and Remissness.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int num1,num2;
+	    cin>>num1>>num2;
+	    int sum = num1+num2;
+	    cout<<max(num1,num2)<<" "<<sum<<endl;
+	}
+	return 0;
+}
