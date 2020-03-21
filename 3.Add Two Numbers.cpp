ebfc63e9d40c7b29c4ca95a87bebb5b50fdf039d
HarshitaSonkar <58362202+HarshitaSonkar@users.noreply.github.com>
2020-03-21 15:31:44 +0530

    Update 3.Add Two Numbers.cpp

--- 3.Add Two Numbers.cpp
+++ 3.Add Two Numbers.cpp
@@ -1,14 +1,11 @@
-// bits/stdc++.h works in linux. 
-// It loads most of the libraries of C++ required.
-#include <bits/stdc++.h> 
 
-using namespace std;
+#include <bits/stdc++.h> using namespace std;
 
 int main() {
 	// Read the number of test cases.
-	int T;
-	scanf("%d", &T);
-	while (T--) {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
 		// Read the input a, b
 		int a, b;
 		scanf("%d %d", &a, &b);
