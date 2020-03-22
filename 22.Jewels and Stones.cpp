6af494eaf5c0dcb480815ae695053dc71f24e668
HarshitaSonkar <58362202+HarshitaSonkar@users.noreply.github.com>
2020-03-22 23:50:42 +0530

    Create 22.Jewels and Stones.cpp

---
+++ 22.Jewels and Stones.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int count=0;
+        string j;
+        string s;
+        cin>>j>>s;
+        map<char,int>m;
+        for(char A:j)
+           m.insert({A,1});
+        for(char A:s)
+           if(m.find(A)!=m.end())
+                  count++;
+                  cout<<count<<endl;
+           
+    }
+	
+	
+	return 0;
+}
