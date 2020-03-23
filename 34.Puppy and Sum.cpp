b573c19a5026a503ea285c2d97fa009e362285db
HarshitaSonkar <58362202+HarshitaSonkar@users.noreply.github.com>
2020-03-23 21:42:16 +0530

    Create 34.Puppy and Sum.cpp

---
+++ 34.Puppy and Sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	     
+	    int d,n;
+	    int sum=0;
+	    cin>>d>>n;
+	    while(d--){
+	        n=n*(n+1)/2;
+	        }
+	     
+	  cout<<n<<endl;  
+	}
+	return 0;
+}
