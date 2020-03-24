7d510adb33fe9f12632e0b339b3c5a0e7c113990
HarshitaSonkar <58362202+HarshitaSonkar@users.noreply.github.com>
2020-03-24 19:59:33 +0530

    Create 37.Chef and the Wildcard Matching.cpp

---
+++ 37.Chef and the Wildcard Matching.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    string x,y;
+	    cin>>x>>y;
+	    bool b=true;
+	    for(int i=0; i<x.size();i++){
+	        if(x[i]==y[i] || x[i]=='?' || y[i]=='?'){
+	            //b=true;
+	            
+	        }else
+	            b=false;
+	            
+	        }
+	        if(b==true)
+	        cout<<"Yes"<<endl;
+	        else
+	        cout<<"No"<<endl;
+	    }
+	
+	return 0;
+}
