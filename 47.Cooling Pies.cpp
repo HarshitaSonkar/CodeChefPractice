9259b5885c60f5c0afedf518c20f4f989b756fa5
HarshitaSonkar <58362202+HarshitaSonkar@users.noreply.github.com>
2020-03-25 19:54:33 +0530

    Create 47.Cooling Pies.cpp

---
+++ 47.Cooling Pies.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int>v1(n,0);
+	    vector<int>v2(n,0);
+	    for(int i=0;i<n;i++){
+	        cin>>v1[i];
+	    }
+	    for(int j=0;j<n;j++){
+	        cin>>v2[j];
+	    }
+	    sort(v1.begin(),v1.end());
+	    sort(v2.begin(),v2.end());
+	    int c=0;
+	    int i=0,j=0;
+	    while(i<n && j<n){
+	        if(v1[i]<=v2[j]){
+	            c=c+1;
+	            i++;
+	            j++;
+	        }else{
+	            j++;
+	        }
+	    }
+	    cout<<c<<endl;
+	}
+	return 0;
+}
