a6db0d41cbd62a958935fe2eee8042d5b8c1e1c6
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-05-07 22:28:02 +0530

    Update 12.Chef and Remissness.cpp

--- 12.Chef and Remissness.cpp
+++ 12.Chef and Remissness.cpp
@@ -5,10 +5,10 @@ int main() {
 	int n;
 	cin>>n;
 	while(n--){
-	    int num1,num2;
-	    cin>>num1>>num2;
-	    int sum = num1+num2;
-	    cout<<max(num1,num2)<<" "<<sum<<endl;
+	     int num1,num2;
+	     cin>>num1>>num2;
+	     int sum = num1+num2;
+	     cout<<max(num1,num2)<<" "<<sum<<endl;
 	}
 	return 0;
 }
